1719625c33a6c284dcead60d684789220daf33f5
Gazmendrx <gazmendrx@bluebotdev.com>
2021-11-06 22:36:06 +0100

    init nextjs base app

---
+++ rx-dev/pages/index.js
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 mx-auto text-center m-5 p-5">
+          <h1>rx-dev.com - Coming soon... </h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 position-absolute bottom-0 start-50 translate-middle text-center">
+          <p>© 2021 - RXDev </p>
+        </div>
+      </div>
+    </div>
+  )
+}
